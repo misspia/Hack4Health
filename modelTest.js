82ce6e32f691e687ec7ed43dfb0c74e47a340234
Kelvin Yang <kelvinyang.is@gmail.com>
2016-11-05 22:24:09 -0400

    CORS

--- modelTest.js
+++ modelTest.js
@@ -3,6 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var models = require('./models/model.js');
@@ -19,6 +29,7 @@ app.get('/getQuestion/:first/:last', function(req, res) {
     var last = req.params.last;
     models.getQuestionByName(first, last, function(data) {
         res.send(data);
+        res.set('Content-Type', 'text/plain');
     });
 });
 
